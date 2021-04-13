a64357c310018d948adc6fd12a4d037417e0ed16
Alberto Kato <albertokatojr@gmail.com>
2021-04-13 14:36:27 -0300

    [FRONTEND] 1) fixing routes 2) Added Buefy 3) Created navBar component

---
+++ front/src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+    <b-navbar>
+        <template #start>
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
+                Home
+            </b-navbar-item>
+            <b-navbar-item tag="router-link" :to="{ path: '/add-vehicle' }">
+                Add Vehicle
+            </b-navbar-item>
+        </template>
+    </b-navbar>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
