9b89a985ede199a1bdac4ba73e44351f865d9bae
Alberto Kato <albertokatojr@gmail.com>
2021-04-13 21:45:08 -0300

    [BACKEND][FIX] query method in baseController not working properly with regex expression

--- back/app/common/baseController.js
+++ back/app/common/baseController.js
@@ -68,13 +68,6 @@ exports.get = (Model) => async (req, res, next) => {
     }
     const { sort, ...filter } = req.query;
     const sortBy = sort ? { [sort]: 1 } : {};
-    Object.keys(filter).map((key) => {
-      if (!key.includes('_id')) {
-        filter[key] = new RegExp(`^.*${filter[key]}.*$`, 'i');
-      }
-      return filter[key];
-    });
-
     const entities = await Model.find(filter).sort(sortBy);
     return res.status(200).json({
       status: 'success',
